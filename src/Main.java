091d2681755d6e2118918e9f47656a1f971a0cc3
dam_lmj <juanvi1921.cursos@gmail.com>
2024-04-23 13:04:36 +0200

    Commit con un metodo en el main gastando el FizzBuzz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        fizzBuzzPrimeros100();
+    }
+    public static void fizzBuzzPrimeros100() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Fizzbuzz.fizzBuzzMetodo(i));
+        }
     }
 }
