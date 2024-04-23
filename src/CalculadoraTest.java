0ad9b0683403f91a29dc2725baecef2a16255250
dam_lmj <juanvi1921.cursos@gmail.com>
2024-04-23 13:29:22 +0200

    Commit con los test 3 y 4 de la calculadora

--- src/CalculadoraTest.java
+++ src/CalculadoraTest.java
@@ -12,4 +12,14 @@ class CalculadoraTest {
     void calculadoraTest2() {
         assertEquals(1, Calculadora.suma("1"));
     }
+
+    @Test
+    void calculadoraTest3() {
+        assertEquals(3, Calculadora.suma("1,2"));
+    }
+
+    @Test
+    void calculadoraTest4() {
+        assertEquals(4, Calculadora.suma("1,1,2"));
+    }
 }
